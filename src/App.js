f1fc29b0e7575eb61e185fca91055e4f82b46cfd
Ahmed A. Shatat <a.shatat@hotmail.com>
2020-10-08 16:55:46 +0300

    modify App.js
    remove the CRA code
    
    #25

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Header from './components/Header';
+import Card from './components/Card';
+import ImagesWrapper from './components/ImagesWrapper';
+
 import './App.css';
 
 function App() {
-  const name = 'ahmed'
   return (
-    <div className="App">
-      my name is {name}
-    </div>
+    <>
+      <div id="aaa">asdfasdf</div>
+      <Header sirName="nabahin" className="header" />
+      <ImagesWrapper>
+        <Card imageUrl="https://static.toiimg.com/photo/72975551.cms"/>
+        <Card imageUrl="https://static.toiimg.com/photo/72975551.cms"/>
+        <Card imageUrl="https://static.toiimg.com/photo/72975551.cms"/>
+        <Card imageUrl="https://static.toiimg.com/photo/72975551.cms"/>
+        <Card imageUrl="https://static.toiimg.com/photo/72975551.cms"/>
+      </ImagesWrapper>
+    </>
   );
 }
 
